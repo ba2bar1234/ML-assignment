8922a0a01f58ebf070c408f267f3f1d89b4c3d0d
ba2bar1234 <108226597+ba2bar1234@users.noreply.github.com>
2023-06-19 11:32:58 +0500

    Update decision.py

---
+++ decision.py
@@ -0,0 +1,28 @@
+# Importing the required libraries
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+# Load the dataset
+dataset = datasets.load_iris()
+
+# Split the dataset into features and target variables
+X = dataset.data
+y = dataset.target
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a decision tree classifier
+classifier = DecisionTreeClassifier()
+
+# Train the classifier on the training data
+classifier.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = classifier.predict(X_test)
+
+# Calculate the accuracy of the classifier
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
